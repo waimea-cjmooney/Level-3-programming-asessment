cb94b6ec270197f70821a07ee0737c8b278b5275
waimea-cjmooney <cjmooney@waimea.school.nz>
2025-03-19 12:07:24 +1300

    Project Set Up

--- src/Main.kt
+++ src/Main.kt
@@ -3,8 +3,8 @@
  * Programming Project for NCEA Level 3, Standard 91906
  * ---------------------------------------------------------------------
  * Project Name:   PROJECT NAME HERE
- * Project Author: PROJECT AUTHOR HERE
- * GitHub Repo:    GITHUB REPO URL HERE
+ * Project Author: Corban Mooney
+ * GitHub Repo:    https://github.com/waimea-cjmooney/Level-3-programming-assessment
  * ---------------------------------------------------------------------
  * Notes:
  * PROJECT NOTES HERE
